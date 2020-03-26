8783af0aab542e117933500a8d7ea23770e279eb
linton-dawson <pushkarkukde@gmail.com>
2020-03-26 11:27:28 +0530

    Added gitignore file and default image opening

--- zscraper.py
+++ zscraper.py
@@ -1,12 +1,16 @@
-import requests,os,bs4
+import requests
+import os
+import bs4
 from PIL import Image
 
 #Using the URL for Random post bar
 url = 'http://zenpencils.com/?randompost'
 
+#Using requests library and beautifulsoup to get relevant html header
 res = requests.get(url)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, features="html5lib")
+
 
 #Get the <img> element under <div> id=comic
 comicElem = soup.select('#comic img')
@@ -17,6 +21,8 @@ else :
 	res = requests.get(comicUrl)
 	res.raise_for_status()
 
+
+#Downloading comic in ./img/ directory
 comicName = os.path.basename(comicUrl)
 print('Downloading comic named %s...' % comicName)
 imageFile = open(os.path.join('img',os.path.basename(comicUrl)), 'wb')
@@ -24,8 +30,9 @@ for chunk in res.iter_content(1000000):
 	imageFile.write(chunk)
 imageFile.close()
 
-#Opening the file using Image function of the PIL module (uses ImageMagick)
-im = Image.open('img/'+ comicName)
-im.show()
 
-print('Done\n')
+#Opening image using xdg-open command which opens in default image viewer
+im = 'xdg-open img/' + comicName
+os.system(im)
+
+print('Download complete !\nOpening image in default image viewer.')
